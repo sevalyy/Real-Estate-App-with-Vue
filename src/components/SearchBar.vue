2a2101dbd961e7ad35d31c97524f505fdb01921e
sevalyy <seval@yamanyar.com>
2022-11-18 12:31:38 +0100

    searchBar component added

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input
+      type="text"
+      placeholder="Search..."
+      v-model.trim="input"
+      v-on:input="search()"
+      ref="input"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  //   name: "listSearch",
+  data: () => ({ input: "" }),
+  methods: {
+    search() {
+      console.log("search is working", this.input);
+    },
+    closeSearchBar() {
+      this.$store.commit("SET_DISPLAY_SEARCH_LIST", false);
+    },
+  },
+};
+</script>
+
+<style></style>
